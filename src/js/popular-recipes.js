a48d944c98ac9d1c6376476ef790e32f0e324349
przystanwrozmyslaniu <iov220291@gmail.com>
2023-09-17 10:23:56 +0200

    popular-recipes-1

---
+++ src/js/popular-recipes.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+
+const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/recipes/popular'
+
+const container = document.querySelector('.js-popular-recipes-list')
+// console.log(container)
+
+const getPopularRecipe = async () => {
+  const response = await axios.get(`${BASE_URL}`)
+  console.log (response.data)
+  return response.data
+}
+
+const recipies = getPopularRecipe()
+// console.log(recipies)
+getPopularRecipe()
+  .then((data) => {
+    if (data.length === 0) {
+      console.log(`Error`)
+      return
+    }
+    //   container.innerHTML = createMarkupPopularRecipies(data)
+      container.insertAdjacentHTML('beforeend', createMarkupPopularRecipies(data))
+  })
+
+container.addEventListener('click', handlerPopularRecipeClick)
+
+function handlerPopularRecipeClick(evt) {
+    if (evt.target === evt.currentTarget) {
+        return
+    }
+
+    const currentPopularRecipe = evt.target.closest('.js-popular-recipes-item')
+    const { _id } = currentPopularRecipe.dataset;
+    // console.log(evt.target);
+    console.log(currentPopularRecipe);
+}
+
+// Markup //
+function createMarkupPopularRecipies(arr) {
+    return arr.map(({ _id, title, description, preview }) =>
+    `<li data-id="${_id}" class="popular-recipes-item js-popular-recipes-item">
+        <img
+            class="popular-recipes-img"
+            src="${preview}"
+            alt="${title}"/>
+        <div class="popular-recepices-text">
+            <h3 class="popular-recipes-subtitle">${title}</h3>
+            <p class="popular-recipes-description">${description}</p>
+            </div>
+    </li>`).join('');
+}
