1fa9bf47cd0ac470b0e19537fdee2170c434abc6
Andrii Horbovskyi <117573529+g0rbochek@users.noreply.github.com>
2023-09-19 18:12:42 +0200

    swipe

--- src/js/swiper/swiper.js
+++ src/js/swiper/swiper.js
@@ -1,51 +1,69 @@
 // import Swiper from 'swiper';
+
 // import { Navigation, Pagination } from 'swiper';
-import Swiper, { Pagination, Navigation, Autoplay } from 'swiper';
-import 'swiper/swiper-bundle.min.css';
+// import Swiper, { Pagination, Navigation, Autoplay } from 'swiper';
+// import 'swiper/swiper-bundle.min.css';
 
 // import '../../node_modules/swiper/swiper-bundle.css';
 
-import { createMarkup } from './swiper-markup';
-import { getFetch } from './swiper-api';
+// import { createMarkup } from './swiper-markup';
+// import { getFetch } from './swiper-api';
 
-const way = document.querySelector('.js-swiper');
+// const way = document.querySelector('.js-swiper');
 // console.log(way);
 // const mySwiper = document.querySelector('.mySwiper');
 // console.log(mySwiper);
 // console.log(getFetch());
 
-const swiper = new Swiper('.mySwiper', {
-  slidesPerView: 3,
-  spaceBetween: 16,
-  allowSlideNext: true,
-  // allowSlideNext: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  autoplay: {
-    delay: 5000,
-  },
-});
 // const swiper = new Swiper('.mySwiper', {
-//   modules: [Pagination, Navigation, Autoplay],
+//   slidesPerView: 3,
+//   spaceBetween: 16,
 //   allowSlideNext: true,
+//   // allowSlideNext: true,
 //   pagination: {
-//     el: '.page',
+//     el: '.swiper-pagination',
 //     clickable: true,
 //   },
 //   autoplay: {
-//     delay: 1500,
+//     delay: 5000,
 //   },
-//   speed: 800,
-//   loop: true,
+// });
+
 // });
 // console.log(swiper);
 
-async function get() {
-  const result = await getFetch();
-  // createMarkup(result);
-  way.insertAdjacentHTML('beforeend', createMarkup(result));
-}
+// async function get() {
+//   const result = await getFetch();
+//   // createMarkup(result);
+//   way.insertAdjacentHTML('beforeend', createMarkup(result));
+// }
 
-get();
+// get();
+// import Swiper from 'swiper';
+// import { Pagination, Autoplay } from 'swiper';
+// import 'swiper/css/pagination';
+// import 'swiper/css';
+
+import Swiper from 'swiper';
+import { Pagination, Autoplay } from 'swiper';
+
+export const swiper = new Swiper('.swiper', {
+  noSwiping: true,
+  noSwipingSelector: '.swiper',
+  speed: 800,
+  slidesPerView: 0.7,
+  spaceBetween: '16px',
+  loop: true,
+  modules: [Pagination, Autoplay],
+  Autoplay: {
+    delay: 500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.pages-pagination',
+    clickable: true,
+  },
+});
+
+// swiper.use([Pagination]);
+swiper.autoplay.start();
