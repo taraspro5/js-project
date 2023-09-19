7434b6340d9821fb6925935d968323119b919160
taraspro5 <kovtun_taras007@ukr.net>
2023-09-19 19:44:25 +0300

    зробив трохи js і додав стилі

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import './js/modal-recipe.js'
-import './js/categories.js'
+import './js/modal-recipe.js';
+import './js/categories.js';
 import './js/main.section.js';
 import './js/popular-recipes.js';
+import './js/hero-favorite.js';
