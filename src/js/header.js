d66ccf30e2155d5febf222ef0fd81cd30a1034aa
Olena-Nechyporenko <127235568+Olena-Nechyporenko@users.noreply.github.com>
2023-09-19 21:23:19 +0300

    fix errors in categories

--- src/js/header.js
+++ src/js/header.js
@@ -3,6 +3,9 @@ const refs = {
     btnBurger: document.querySelector('.js-burger-menu'),
     btnCloseModal: document.querySelector('.js-btn-close'),
     modal: document.querySelector('.header-back'),
+    homeBtn: document.querySelector('.js-header-ref-home'),
+    favorBtn: document.querySelector('.js-header-ref-favor'),
+    navList: document.querySelector(".header-refs")
   };
   
   refs.btnBurger.addEventListener('click', handlerClickerAdd);
@@ -17,4 +20,21 @@ const refs = {
     refs.modal.classList.toggle('display-none');
     document.body.style.overflow = 'visible';
   }
-  
+
+  refs.navList.addEventListener("click", onNavClick)
+
+  function onNavClick(evt) {
+     if(evt.target === refs.favorBtn) {
+       refs.favorBtn.classList.add("header-active")
+      refs.homeBtn.classList.remove("header-active")
+     }
+     if(evt.target === refs.homeBtn) {
+      refs.homeBtn.classList.add("header-active")
+      refs.favorBtn.classList.remove("header-active")
+     }
+  }
+
+ 
+
+
+  refs.btnCart.addEventListener('click', toggleModal)
